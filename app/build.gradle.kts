7455188ca04592020b7e90a1b097074bad0d37a6
victor <vamvlwfmpmf@gmail.com>
2025-04-07 13:35:57 +0200

    google-services.json cambiado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     alias(libs.plugins.crashlytics)
 }
 
-val packageName = "org.linphone"
+val packageName = "es.mundosms.mundophone"
 val useDifferentPackageNameForDebugBuild = false
 
 val sdkPath = providers.gradleProperty("LinphoneSdkBuildDir").get()
